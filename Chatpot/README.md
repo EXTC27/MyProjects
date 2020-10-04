eda43dffa05c486120ee72d637a2341203c936d7
SinJ <johankim27@gmail.com>
2020-10-04 13:30:52 +0900

    폴더명 수정

--- Chatpot/README.md
+++
@@ -1,3 +0,0 @@
-# ChatPot (작성예정)
-
-- ChatPot Final Report를 참고해 주세요.
--- Chatpot/소스코드/Chatting_Engine/CE_DATA_PlantStandardDB.py
+++
@@ -1,3 +0,0 @@
-#산세베리아
-plant_DB = {'Sansevieria':{'Temperature':[21, 25], 'Ground':[40, 50], 'Light':[300, 10000]}
-           }
